8e9d10220d8e4c85ef179782b48e41e265747527
zhanghaoxu <872297298@qq.com>
2020-04-06 21:50:45 +0800

    登录模块

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
-export default function LoginScreen(props) {
-  return (
-    <View style={styles.container}>
-      <Text>LOINg</Text>
-    </View>
-  );
+import {TextInput, Button} from 'react-native-paper';
+export default class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>登录</Text>
+        <TextInput
+          label="Nick Name"
+          value={this.state.text}
+          onChangeText={text => this.setState({text})}
+        />
+        <TextInput
+          label="Email"
+          value={this.state.text}
+          onChangeText={text => this.setState({text})}
+        />
+        <TextInput
+          label="password"
+          secureTextEntry={true}
+          value={this.state.text}
+          onChangeText={text => this.setState({text})}
+        />
+        <Button
+          style={styles.login}
+          icon="camera"
+          mode="contained"
+          onPress={() => console.log('Pressed')}>
+          Press me
+        </Button>
+      </View>
+    );
+  }
 }
 
 LoginScreen.navigationOptions = {
@@ -15,5 +49,9 @@ LoginScreen.navigationOptions = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
+  },
+  login: {
+    marginTop: 30,
   },
 });
