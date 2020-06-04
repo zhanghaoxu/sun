b87e13c507bcdb95c56bc219eba31dac91bb4160
zhanghaoxu <872297298@qq.com>
2020-06-04 23:16:43 +0800

    ss

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ import theme from '@/constants/Theme';
 import {BackHandler} from 'react-native';
 import {EXIT_APP_NEED_CONFIRM} from '@/constants/Config';
 import backActionConfirmHandler from '@/utils/backActionConfirmHandler';
-
+import JPush from 'jpush-react-native';
 export default class App extends React.Component {
   componentDidMount() {
     SplashScreen.hide();
@@ -22,6 +22,38 @@ export default class App extends React.Component {
         'hardwareBackPress',
         backActionConfirmHandler,
       );
+
+    JPush.init();
+    //连接状态
+    this.connectListener = result => {
+      console.log('connectListener:' + JSON.stringify(result));
+    };
+    JPush.addConnectEventListener(this.connectListener);
+    //通知回调
+    this.notificationListener = result => {
+      console.log('notificationListener:' + JSON.stringify(result));
+    };
+    JPush.addNotificationListener(this.notificationListener);
+    //本地通知回调
+    this.localNotificationListener = result => {
+      console.log('localNotificationListener:' + JSON.stringify(result));
+    };
+    JPush.addLocalNotificationListener(this.localNotificationListener);
+    //自定义消息回调
+    this.customMessageListener = result => {
+      console.log('customMessageListener:' + JSON.stringify(result));
+    };
+    JPush.addCustomMessagegListener(this.customMessageListener);
+    //tag alias事件回调
+    this.tagAliasListener = result => {
+      console.log('tagAliasListener:' + JSON.stringify(result));
+    };
+    JPush.addTagAliasListener(this.tagAliasListener);
+    //手机号码事件回调
+    this.mobileNumberListener = result => {
+      console.log('mobileNumberListener:' + JSON.stringify(result));
+    };
+    JPush.addMobileNumberListener(this.mobileNumberListener);
   }
 
   componentWillUnmount() {
