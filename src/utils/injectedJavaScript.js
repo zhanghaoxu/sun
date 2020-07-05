b972eadc7eb023c1a7fb86dffd4be894e293a7d0
zhanghaoxu <872297298@qq.com>
2020-07-05 20:42:48 +0800

    脚本修改

--- src/utils/injectedJavaScript.js
+++ src/utils/injectedJavaScript.js
@@ -3,17 +3,17 @@ export default function() {
     function wrap(fn) {
       return function wrapper() {
         var res = fn.apply(this, arguments);
-        window.ReactNativeWebView.postMessage("navigationStateChange");
+        window.ReactNativeWebView.postMessage('navigationStateChange');
         return res;
       };
     }
 
-    history.pushState = wrap(history.pushState);
-    history.replaceState = wrap(history.replaceState);
-    window.addEventListener("popstate", function() {
-      window.ReactNativeWebView.postMessage("navigationStateChange");
+    window.history.pushState = wrap(window.history.pushState);
+    window.history.replaceState = wrap(window.history.replaceState);
+    window.addEventListener('popstate', function() {
+      window.ReactNativeWebView.postMessage('navigationStateChange');
     });
 
-    window.dispatchEvent(new Event("InjectReactNativeWebViewSuccess"));
+    window.dispatchEvent(new Event('InjectReactNativeWebViewSuccess'));
   })();
 }
